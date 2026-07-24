6d59b8683cefcf06344ad6338e4504755a98db10
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-07-24 10:16:43 +1000

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = [True] * len(nums)
+        def dfs(i):
+            nonlocal memo
+            if i + 1 >= len(nums):
+                return True
+            
+            for j in range(nums[i], 0, - 1):
+                if memo[i] and dfs(i + j):
+                    return True
+
+            memo[i] = False
+            return False
+
+        return dfs(0)
