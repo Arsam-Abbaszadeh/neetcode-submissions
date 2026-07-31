07fe0cfc7e52a4eab088580627c205b552afe50e
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-07-31 20:36:25 +1000

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        prev2 = nums[0]
+        prev = max(nums[0], nums[1])
+    
+        for i in range(2, n):
+            if prev == prev2:
+                temp = prev
+                prev2 = prev
+                prev = temp + nums[i]
+            elif nums[i] + prev2 > prev:
+                temp = prev2
+                prev2 = prev
+                prev = nums[i] + temp
+            elif nums[i] > nums[i - 1]:
+                prev2 = prev
+                prev += (nums[i] - nums[i - 1])
+            else:
+                prev2 = prev
+
+        return prev
