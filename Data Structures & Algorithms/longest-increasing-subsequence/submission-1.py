fbd7a0eee49258b6dd62105cefd7712583ac9eca
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-03 11:24:04 +1000

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # DFS with DP
+        dp = [None] * len(nums)
+        dp[-1] = 1
+        max_len, n = 1, len(nums)
+        def dfs(i):
+            if i + 1 >= len(nums):
+                return # did not contribute to the length of the longest subsequence
+            nonlocal max_len
+            if dp[i] != None:
+                return
+
+            joined_seq = False
+            for j in range(i + 1, n):
+                dfs(j)
+                if nums[j] > nums[i]:
+                    dp[i] = max(dp[i] or 0, dp[j] + 1)
+                    joined_seq = True
+            if not joined_seq:
+                dp[i] = 1
+        dfs(0)
+        print(dp)
+        return max(dp)
