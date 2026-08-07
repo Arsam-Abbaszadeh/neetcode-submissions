c6c4c41880757159a5cff617c4f8dc30becd4a38
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-07 11:48:48 +1000

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        n, m = len(grid), len(grid[0])
+        avoid = (-1, 0)
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 0:
+                    # start BFS
+                    dist = 0
+                    q = deque([(i ,j)])
+                    while q:
+                        level = len(q)
+                        for _ in range(level):
+                            r, c = q.popleft()
+                            if (grid[r][c] == 0 and dist <= grid[r][c]
+                                or grid[r][c] != -1 and dist < grid[r][c]) :
+                                grid[r][c] = dist
+                                if r > 0:
+                                    q.append((r - 1, c))
+                                if r < n - 1:
+                                    q.append((r + 1, c))
+                                if c > 0:
+                                    q.append((r, c - 1))
+                                if c < m - 1:
+                                    q.append((r, c + 1))
+                        dist += 1
+                            
+
