e9c0892370cee3eaf67c258768969984ef27f5fd
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-14 16:13:42 +1000

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,24 @@
+import heapq
+from collections import defaultdict
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if groupSize == 1 and len(hand) > 0:
+            return True
+            
+        groups = defaultdict(list)
+        hand.sort()
+        groups_finished = 0
+        for num in hand:
+            new_size = -1
+            if (num - 1) in groups and len(groups[num - 1]) > 0:
+                # insert into heap handler
+                new_size = heapq.heappop(groups[num - 1]) - 1
+                if -new_size == groupSize:
+                    groups_finished += 1
+                    continue
+                    
+            heapq.heappush(groups[num], new_size)
+
+
+        return groups_finished * groupSize == len(hand)
+
