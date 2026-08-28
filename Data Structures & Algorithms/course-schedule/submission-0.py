65740eda8c0041a0d3c63fe0394d42b6fd189dbe
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-29 08:57:01 +1000

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #  Khans algorithm
+
+        # build an adjacency list
+        adj_list = [[] for _ in range(numCourses)]
+        in_edges = [0 for _ in range(numCourses)]
+        for c2, c1 in prerequisites:
+            adj_list[c1].append(c2)
+            in_edges[c2] += 1
+        
+        # get nodes with 0 incoming
+        completed = 0
+        q = deque()
+        for i, in_list in enumerate(in_edges):
+            if in_list == 0:
+                q.append(i)
+                completed += 1
+        
+        # vis = set()
+        while q:
+            cur = q.popleft()
+            for child in adj_list[cur]:
+                in_edges[child] -= 1
+                if in_edges[child] == 0:
+                    q.append(child) 
+                    completed += 1
+        
+        return completed == numCourses
+            
+            
+
+
