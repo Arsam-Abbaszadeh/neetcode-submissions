ad25cc2eaa165daee491b8999239634a5cd09f53
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-30 13:50:34 +1000

    Add: ones-and-zeroes - submission-6

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        # bottom up approach space optimized
+        L = len(strs)
+        memo = {}
+
+        def mnCount(s: str) -> tuple[int, int]:
+            m = n = 0
+            for c in s:
+                if c == '0':
+                    m += 1
+                else:
+                    n += 1
+            return m, n
+
+        largestSub = 0
+        memo[(0, 0)] = 0
+        for s in strs:
+            new_memo = memo.copy()
+            cm, cn = mnCount(s)
+            for (tm, tn), subC in memo.items():
+                if tm + cm <= m and tn + cn <= n:
+                    oldVal = new_memo.get((tm + cm, tn + cn), 0)
+                    new_memo[(tm + cm, tn + cn)] = max(oldVal, subC + 1)
+                    largestSub = max(largestSub, subC + 1, oldVal)
+            memo = new_memo
+            
+        return largestSub
+
+
+
