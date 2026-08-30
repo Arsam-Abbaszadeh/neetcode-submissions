0b926618f9c900ad3cd2cd4984bcbcd32dba88ca
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-08-30 20:09:12 +1000

    Add: n-queens - submission-8

---
+++ Data Structures & Algorithms/n-queens/submission-8.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.'] * n for _ in range(n)]
+        ans = []
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        def backtrack(r, rem, board):
+            if rem == 0:
+                ans.append(["".join(r) for r in board])
+                return 
+
+            for c in range(n):
+                p_diag = c + r
+                n_diag = c - r
+                if c not in cols and p_diag not in posDiag and n_diag not in negDiag:
+                    board[r][c] = 'Q'
+                    cols.add(c)
+                    negDiag.add(n_diag)
+                    posDiag.add(p_diag)
+
+                    backtrack(r + 1, rem - 1, board)
+
+                    cols.remove(c)
+                    negDiag.remove(n_diag)
+                    posDiag.remove(p_diag)
+                    board[r][c] = '.'
+
+        backtrack(0, n, board)
+        return ans   
