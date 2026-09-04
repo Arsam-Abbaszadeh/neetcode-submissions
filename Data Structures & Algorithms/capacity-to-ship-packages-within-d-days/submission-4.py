a0eda1934dbe10a56b50d204f9d5cd6c756a1dfd
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-09-04 18:42:25 +1000

    Add: capacity-to-ship-packages-within-d-days - submission-4

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # find max upper limit
+        potentialPerDayLimit = math.ceil(sum(weights) / days)
+        packagesPerDay = math.ceil(len(weights) / days)
+        total = 0
+        i = 0
+        while i < packagesPerDay:
+            total += weights[i]
+            i += 1
+        potentialPerDayLimit = max(total, potentialPerDayLimit)
+        for j in range(i, len(weights)):
+            total += weights[j] - weights[j - packagesPerDay]
+            potentialPerDayLimit = max(total, potentialPerDayLimit)
+
+        print(potentialPerDayLimit) 
+        # greedy check if limit is possible
+        def validLimit(limit) -> bool:
+            day = 1
+            p = 0
+            while day <= days and p < len(weights) and weights[p] <= limit:
+                loaded = 0
+                while p < len(weights) and loaded + weights[p] <= limit:
+                    loaded += weights[p]
+                    p += 1
+                day += 1
+                
+            return day - 1 <= days and p == len(weights)
+
+        #  binary search for lowest limit
+        l = 0
+        r = potentialPerDayLimit
+        prev = -1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if validLimit(mid):
+                prev = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return prev
