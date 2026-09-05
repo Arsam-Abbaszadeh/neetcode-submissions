4f9a933fb01bfe8ae1f22a6b9ae4df1db639219b
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-09-05 12:55:54 +1000

    Add: extra-characters-in-a-string - submission-2

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        # brute force backtracking approach
+        dictionary = set(dictionary)
+        dp ={}
+        def dfs(i, j):
+            if j > len(s) or i == len(s):
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            #  2 sets of decisions. Choose to skip and choose not to skip
+            keep = 0
+            if s[i : j] in dictionary:
+                keep = dfs(j, j + 1) + (j - i)
+            
+            expand = dfs(i, j + 1)
+
+            skip = dfs(i + 1, i + 2)
+            dp[(i, j)] = max(expand, keep, skip)
+            return max(expand, keep, skip)
+        
+        return len(s) - dfs(0, 1)
