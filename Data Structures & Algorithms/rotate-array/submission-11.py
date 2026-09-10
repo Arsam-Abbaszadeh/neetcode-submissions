10fd90353f231860a4f1ef2f4ee5064d3f19a57e
Arsam Abbaszadeh <101549983+Arsam-Abbaszadeh@users.noreply.github.com>
2026-09-10 12:45:46 +1000

    Add: rotate-array - submission-11

---
+++ Data Structures & Algorithms/rotate-array/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # reverse whole array an then reverse then reverse 0 - (k - 1) and k - (n - 1)
+        k = k % len(nums)
+        def swapSubArray(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        swapSubArray(0, len(nums) - 1)
+        swapSubArray(0, k - 1)
+        swapSubArray(k, len(nums) - 1)
+        
+    
